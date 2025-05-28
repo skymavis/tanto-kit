9a8d12b91e3aaa0d6e26cf94f1f560a19b500b05
Nguyen Huu Gia Tri <44458003+nguyenhuugiatri@users.noreply.github.com>
2025-05-28 14:38:24 +0700

    feat: add source config (#52)
    * feat: add source config
    
    * chore: install

--- packages/connect/src/providers/waypoint-provider.ts
+++ packages/connect/src/providers/waypoint-provider.ts
@@ -2,6 +2,7 @@ import { WaypointProvider } from '@sky-mavis/waypoint';
 
 import { ChainIds } from '../common/chain';
 import { ConnectorError, ConnectorErrorType } from '../types/connector-error';
+import { getVersionInfo } from '../utils';
 
 export type WaypointScope = 'openid' | 'profile' | 'email' | 'wallet';
 
@@ -11,10 +12,11 @@ export interface IWaypointProviderConfigs {
   waypointOrigin?: string;
   scopes?: WaypointScope[];
   popupCloseDelay?: number;
+  source?: string;
 }
 
 export const requestWaypointProvider = (configs?: IWaypointProviderConfigs) => {
-  const { chainId, clientId, waypointOrigin, scopes, popupCloseDelay } = configs || {};
+  const { chainId, clientId, waypointOrigin, scopes, popupCloseDelay, source } = configs || {};
 
   const waypointProvider = WaypointProvider.create({
     clientId: clientId as string,
@@ -22,6 +24,7 @@ export const requestWaypointProvider = (configs?: IWaypointProviderConfigs) => {
     waypointOrigin,
     scopes,
     popupCloseDelay,
+    source: source ?? getVersionInfo(),
   });
 
   if (!waypointProvider) {
