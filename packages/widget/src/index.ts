1a1b5eedb87dd87593104a508a974586e988f529
nguyenhuugiatri <nguyenhuugiatri@gmail.com>
2025-05-30 14:16:59 +0700

    fix: exports

--- packages/widget/src/index.ts
+++ packages/widget/src/index.ts
@@ -5,18 +5,7 @@ export { TantoProvider } from './contexts/tanto/TantoProvider';
 export type { TantoEmbeddedWidgetProps } from './EmbeddedWidget';
 export { TantoEmbeddedWidget } from './EmbeddedWidget';
 export type { AppMetadata, DefaultConfig, KeylessWalletConfig } from './getDefaultConfig';
-export {
-  createAppMetadata,
-  createCoinbaseConnector,
-  createConnectors,
-  createRoninConnector,
-  createSafeConnector,
-  createTransports,
-  createWalletConnectConnector,
-  createWaypointConnector,
-  getDefaultConfig,
-  RONIN_WALLET_METADATA,
-} from './getDefaultConfig';
+export { createConnectors, createTransports, getDefaultConfig, RONIN_WALLET_METADATA } from './getDefaultConfig';
 export { useConnectedWallet } from './hooks/useConnectedWallet';
 export type { UseRnsAddressParameters } from './hooks/useRnsAddress';
 export { useRnsAddress } from './hooks/useRnsAddress';
