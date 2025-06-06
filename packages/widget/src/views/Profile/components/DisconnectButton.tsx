707293a5e578ad599a2e22d5370adf10bc5db5bf
nguyenhuugiatri <nguyenhuugiatri@gmail.com>
2025-06-06 15:59:29 +0700

    fix: mobile view disconnect

--- packages/widget/src/views/Profile/components/DisconnectButton.tsx
+++ packages/widget/src/views/Profile/components/DisconnectButton.tsx
@@ -2,16 +2,18 @@ import { useCallback, useContext } from 'react';
 import { useDisconnect } from 'wagmi';
 
 import { Button } from '../../../components/button/Button';
-import { VISIBILITY_TRANSITION_DURATION } from '../../../constants';
+import { DIALOG_VISIBILITY_TRANSITION_DURATION, DRAWER_VISIBILITY_TRANSITION_DURATION } from '../../../constants';
 import { WidgetModalContext } from '../../../contexts/widget-modal/WidgetModalContext';
+import { useIsMobileView } from '../../../hooks/useIsMobileView';
 
 export function DisconnectButton() {
+  const isMobile = useIsMobileView();
   const { disconnect } = useDisconnect();
   const widgetModalContext = useContext(WidgetModalContext);
 
   const handleDisconnect = useCallback(() => {
     if (widgetModalContext) widgetModalContext.hide();
-    setTimeout(disconnect, VISIBILITY_TRANSITION_DURATION);
+    setTimeout(disconnect, isMobile ? DRAWER_VISIBILITY_TRANSITION_DURATION : DIALOG_VISIBILITY_TRANSITION_DURATION);
   }, [widgetModalContext, disconnect]);
 
   return (
